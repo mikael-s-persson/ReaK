9abbbbc205b7aa9be2d14d6c844ed0d50d4e0664
mikael <mikael.s.persson@gmail.com>
2013-01-12 20:36:49 -0500

    Finished the Ndof SAP-vp implementation.

--- src/ReaK/ctrl/interpolation/sustained_acceleration_pulse_Ndof_detail.hpp
+++ src/ReaK/ctrl/interpolation/sustained_acceleration_pulse_Ndof_detail.hpp
@@ -356,19 +356,15 @@ namespace detail {
     if(descrim > 0.0) {
       // then there is a real root to the quadratic equation.
       peak_velocity = 0.5 * (v0_v1_ts_sa + sqrt(descrim));
-      if((peak_velocity * sign_p1_p0 < start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 < end_velocity * sign_p1_p0)) {
+      if((peak_velocity * sign_p1_p0 <= start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 <= end_velocity * sign_p1_p0)) {
         return;
       };
       peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
-      if((peak_velocity * sign_p1_p0 < start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 < end_velocity * sign_p1_p0)) {
+      if((peak_velocity * sign_p1_p0 <= start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 <= end_velocity * sign_p1_p0)) {
         return;
       };
       // then, there was no suitable root in this region.
     };
-    
-    
-    
-    
     
     
     // if this point is reached, we have to assume that at least one of the acceleration pulse is too short.
@@ -379,39 +375,20 @@ namespace detail {
     //           + vp/v_max (dt - 2*fabs(vp - v0) - fabs(vp - v1) - a_max)
     //           + (fabs(vp - v1) + a_max) (vp + v1) / 2*v_max
     
-    
-    
-    
-    // try to assume that fabs(a1) = fabs(vp - v1) and fabs(a0) = a_max.
-    // giving:
-    //  p1 - p0 == (fabs(vp - v0) + a_max) (vp + v0) / 2*v_max
-    //           + vp/v_max (dt - fabs(vp - v0) - 2*fabs(vp - v1) - a_max)
-    //           + fabs(vp - v1) (vp + v1) / v_max 
-    
-    
-    // try to assume that fabs(a0) = fabs(vp - v0) and fabs(a1) = fabs(vp - v1).
-    // giving:
-    //  p1 - p0 == fabs(vp - v0) (vp + v0) / v_max 
-    //           + vp/v_max (dt - 2*fabs(vp - v0) - 2*fabs(vp - v1))
-    //           + fabs(vp - v1) (vp + v1) / v_max 
-    
-    
-    
-    
     // try to assume that vp more in the direction (p1-p0) than both v1 and v0 (i.e., ramp-up and ramp-down).
-    double v0_v1_ts = start_velocity + end_velocity + delta_time * sign_p1_p0;
-    double vm_p1_p0 = max_velocity * fabs(end_position - start_position);
-    double vsqr_avg = 0.5 * (start_velocity * start_velocity + end_velocity * end_velocity);
-    if(v0_v1_ts * v0_v1_ts > 4.0 * (vm_p1_p0 + vsqr_avg)) {
+    v0_v1_ts_sa = (2.0 * start_velocity + end_velocity + (delta_time - 0.5 * max_acceleration) * sign_p1_p0) * 2.0 / 3.0;
+    vm_p1_p0 = max_velocity * fabs(end_position - start_position) * 2.0 / 3.0;
+    a_v0_v1_2 = max_acceleration * end_velocity * sign_p1_p0 / 3.0;
+    vsqr_avg = (2.0 * start_velocity * start_velocity + end_velocity * end_velocity) / 3.0;
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg - a_v0_v1_2 + vm_p1_p0);
+    if(descrim > 0.0) {
       // then there is a real root to the quadratic equation.
-      double r1 = 0.5 * (v0_v1_ts + sqrt(v0_v1_ts * v0_v1_ts - 4.0 * (vm_p1_p0 + vsqr_avg))) * sign_p1_p0;
-      if((r1 > start_velocity * sign_p1_p0) && (r1 > end_velocity * sign_p1_p0)) {
-        peak_velocity = sign_p1_p0 * r1;
+      peak_velocity = 0.5 * (v0_v1_ts_sa + sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
         return;
       };
-      r1 = 0.5 * (v0_v1_ts - sqrt(v0_v1_ts * v0_v1_ts - 4.0 * (vm_p1_p0 + vsqr_avg))) * sign_p1_p0;
-      if((r1 > start_velocity * sign_p1_p0) && (r1 > end_velocity * sign_p1_p0)) {
-        peak_velocity = sign_p1_p0 * r1;
+      peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
         return;
       };
       // then, there was no suitable root in this region.
@@ -420,22 +397,189 @@ namespace detail {
     // try to assume that vp is somewhere between v1 and v0 (i.e., ramp-up ramp-up or ramp-down ramp-down).
     if(end_velocity > start_velocity) {
       // ramp-up ramp-up
-      v0_v1_ts = start_velocity - end_velocity + delta_time;
+      v0_v1_ts_sa = 2.0 * (2.0 * start_velocity - end_velocity + delta_time - 0.5 * max_acceleration);
+      vm_p1_p0 = 2.0 * max_velocity * (end_position - start_position);
+      a_v0_v1_2 = max_acceleration * end_velocity;
+      vsqr_avg = 2.0 * start_velocity * start_velocity - end_velocity * end_velocity;
+      descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg - a_v0_v1_2 + vm_p1_p0);
+      if(descrim > 0.0) {
+        peak_velocity = (v0_v1_ts_sa + sqrt(descrim)) * 0.5;
+        if((peak_velocity >= start_velocity) && (peak_velocity <= end_velocity)) {
+          return;
+        };
+        peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+        if((peak_velocity >= start_velocity) && (peak_velocity <= end_velocity)) {
+          return;
+        };
+        // then, the solution doesn't fit the assumption.
+      };
+    } else {
+      // ramp-down ramp-down
+      v0_v1_ts_sa = 2.0 * (end_velocity - 2.0 * start_velocity + delta_time - 0.5 * max_acceleration);
+      vm_p1_p0 = 2.0 * max_velocity * (end_position - start_position);
+      a_v0_v1_2 = max_acceleration * end_velocity;
+      vsqr_avg = 2.0 * start_velocity * start_velocity - end_velocity * end_velocity;
+      descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg + a_v0_v1_2 - vm_p1_p0);
+      if(descrim > 0.0) {
+        peak_velocity = 0.5 * (-v0_v1_ts_sa + sqrt(descrim));
+        if((peak_velocity <= start_velocity) && (peak_velocity >= end_velocity)) {
+          return;
+        };
+        peak_velocity = 0.5 * (-v0_v1_ts_sa - sqrt(descrim));
+        if((peak_velocity <= start_velocity) && (peak_velocity >= end_velocity)) {
+          return;
+        };
+        // then, the solution doesn't fit the assumption.
+      };
+    };
+    
+    // try to assume (the last case) that vp is less in the direction (p1-p0) and both v0 and v1.
+    v0_v1_ts_sa = (-2.0 * start_velocity - end_velocity + (delta_time - 0.5 * max_acceleration) * sign_p1_p0) * 2.0 / 3.0;
+    vm_p1_p0 = max_velocity * fabs(end_position - start_position) * 2.0 / 3.0;
+    a_v0_v1_2 = max_acceleration * end_velocity * sign_p1_p0 / 3.0;
+    vsqr_avg = (2.0 * start_velocity * start_velocity + end_velocity * end_velocity) / 3.0;
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg + a_v0_v1_2 - vm_p1_p0);
+    if(descrim > 0.0) {
+      // then there is a real root to the quadratic equation.
+      peak_velocity = 0.5 * (-v0_v1_ts_sa + sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity <= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity <= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      peak_velocity = 0.5 * (-v0_v1_ts_sa - sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity <= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity <= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      // then, there was no suitable root in this region.
+    };
+    
+    
+    // try to assume that fabs(a1) = fabs(vp - v1) and fabs(a0) = a_max.
+    // giving:
+    //  p1 - p0 == (fabs(vp - v0) + a_max) (vp + v0) / 2*v_max
+    //           + vp/v_max (dt - fabs(vp - v0) - 2*fabs(vp - v1) - a_max)
+    //           + fabs(vp - v1) (vp + v1) / v_max 
+    
+    // try to assume that vp more in the direction (p1-p0) than both v1 and v0 (i.e., ramp-up and ramp-down).
+    v0_v1_ts_sa = (2.0 * end_velocity + start_velocity + (delta_time - 0.5 * max_acceleration) * sign_p1_p0) * 2.0 / 3.0;
+    vm_p1_p0 = max_velocity * fabs(end_position - start_position) * 2.0 / 3.0;
+    a_v0_v1_2 = max_acceleration * start_velocity * sign_p1_p0 / 3.0;
+    vsqr_avg = (2.0 * end_velocity * end_velocity + start_velocity * start_velocity) / 3.0;
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg - a_v0_v1_2 + vm_p1_p0);
+    if(descrim > 0.0) {
+      // then there is a real root to the quadratic equation.
+      peak_velocity = 0.5 * (v0_v1_ts_sa + sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      // then, there was no suitable root in this region.
+    };
+    
+    // try to assume that vp is somewhere between v1 and v0 (i.e., ramp-up ramp-up or ramp-down ramp-down).
+    if(end_velocity < start_velocity) {
+      // ramp-down ramp-down
+      v0_v1_ts_sa = 2.0 * (2.0 * end_velocity - start_velocity + delta_time - 0.5 * max_acceleration);
+      vm_p1_p0 = 2.0 * max_velocity * (end_position - start_position);
+      a_v0_v1_2 = max_acceleration * start_velocity;
+      vsqr_avg = 2.0 * end_velocity * end_velocity - start_velocity * start_velocity;
+      descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg - a_v0_v1_2 + vm_p1_p0);
+      if(descrim > 0.0) {
+        peak_velocity = (v0_v1_ts_sa + sqrt(descrim)) * 0.5;
+        if((peak_velocity >= end_velocity) && (peak_velocity <= start_velocity)) {
+          return;
+        };
+        peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+        if((peak_velocity >= end_velocity) && (peak_velocity <= start_velocity)) {
+          return;
+        };
+        // then, the solution doesn't fit the assumption.
+      };
+    } else {
+      // ramp-up ramp-up
+      v0_v1_ts_sa = 2.0 * (start_velocity - 2.0 * end_velocity + delta_time - 0.5 * max_acceleration);
+      vm_p1_p0 = 2.0 * max_velocity * (end_position - start_position);
+      a_v0_v1_2 = max_acceleration * start_velocity;
+      vsqr_avg = 2.0 * end_velocity * end_velocity - start_velocity * start_velocity;
+      descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg + a_v0_v1_2 - vm_p1_p0);
+      if(descrim > 0.0) {
+        peak_velocity = 0.5 * (-v0_v1_ts_sa + sqrt(descrim));
+        if((peak_velocity >= end_velocity) && (peak_velocity <= start_velocity)) {
+          return;
+        };
+        peak_velocity = 0.5 * (-v0_v1_ts_sa - sqrt(descrim));
+        if((peak_velocity >= end_velocity) && (peak_velocity <= start_velocity)) {
+          return;
+        };
+        // then, the solution doesn't fit the assumption.
+      };
+    };
+    
+    // try to assume (the last case) that vp is less in the direction (p1-p0) and both v0 and v1.
+    v0_v1_ts_sa = (-2.0 * end_velocity - start_velocity + (delta_time - 0.5 * max_acceleration) * sign_p1_p0) * 2.0 / 3.0;
+    vm_p1_p0 = max_velocity * fabs(end_position - start_position) * 2.0 / 3.0;
+    a_v0_v1_2 = max_acceleration * start_velocity * sign_p1_p0 / 3.0;
+    vsqr_avg = (2.0 * end_velocity * end_velocity + start_velocity * start_velocity) / 3.0;
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg + a_v0_v1_2 - vm_p1_p0);
+    if(descrim > 0.0) {
+      // then there is a real root to the quadratic equation.
+      peak_velocity = 0.5 * (-v0_v1_ts_sa + sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity <= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity <= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      peak_velocity = 0.5 * (-v0_v1_ts_sa - sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity <= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity <= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      // then, there was no suitable root in this region.
+    };
+    
+    
+    // try to assume that fabs(a0) = fabs(vp - v0) and fabs(a1) = fabs(vp - v1).
+    // giving:
+    //  p1 - p0 == fabs(vp - v0) (vp + v0) / v_max 
+    //           + vp/v_max (dt - 2*fabs(vp - v0) - 2*fabs(vp - v1))
+    //           + fabs(vp - v1) (vp + v1) / v_max 
+    
+    // try to assume that vp more in the direction (p1-p0) than both v1 and v0 (i.e., ramp-up and ramp-down).
+    v0_v1_ts_sa = start_velocity + end_velocity + 0.5 * delta_time * sign_p1_p0;
+    vm_p1_p0 = 0.5 * max_velocity * fabs(end_position - start_position);
+    vsqr_avg = 0.5 * (start_velocity * start_velocity + end_velocity * end_velocity);
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg + vm_p1_p0);
+    if(descrim > 0.0) {
+      // then there is a real root to the quadratic equation.
+      peak_velocity = 0.5 * (v0_v1_ts_sa + sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+      if((sign_p1_p0 * peak_velocity >= start_velocity * sign_p1_p0) && (sign_p1_p0 * peak_velocity >= end_velocity * sign_p1_p0)) {
+        return;
+      };
+      // then, there was no suitable root in this region.
+    };
+    
+    // try to assume that vp is somewhere between v1 and v0 (i.e., ramp-up ramp-up or ramp-down ramp-down).
+    if(end_velocity > start_velocity) {
+      // ramp-up ramp-up
+      v0_v1_ts_sa = 2.0 * start_velocity - 2.0 * end_velocity + delta_time;
       vm_p1_p0 = max_velocity * (end_position - start_position);
-      vsqr_avg = 0.5 * (start_velocity * start_velocity - end_velocity * end_velocity);
-      if(fabs(v0_v1_ts) > 1e-6 * max_velocity) {
-        peak_velocity = (vm_p1_p0 + vsqr_avg) / v0_v1_ts;
+      vsqr_avg = start_velocity * start_velocity - end_velocity * end_velocity;
+      if(fabs(v0_v1_ts_sa) > 1e-6 * max_velocity) {
+        peak_velocity = (vm_p1_p0 + vsqr_avg) / v0_v1_ts_sa;
         if((peak_velocity >= start_velocity) && (peak_velocity <= end_velocity))
           return;
         // then, the solution doesn't fit the assumption.
       };
     } else {
       // ramp-down ramp-down
-      v0_v1_ts = end_velocity - start_velocity + delta_time;
+      v0_v1_ts_sa = 2.0 * end_velocity - 2.0 * start_velocity + delta_time;
       vm_p1_p0 = max_velocity * (end_position - start_position);
-      vsqr_avg = 0.5 * (end_velocity * end_velocity - start_velocity * start_velocity);
-      if(fabs(v0_v1_ts) > 1e-6 * max_velocity) {
-        peak_velocity = (vm_p1_p0 + vsqr_avg) / v0_v1_ts;
+      vsqr_avg = end_velocity * end_velocity - start_velocity * start_velocity;
+      if(fabs(v0_v1_ts_sa) > 1e-6 * max_velocity) {
+        peak_velocity = (vm_p1_p0 + vsqr_avg) / v0_v1_ts_sa;
         if((peak_velocity >= end_velocity) && (peak_velocity <= start_velocity))
           return;
         // then, the solution doesn't fit the assumption.
@@ -443,23 +587,23 @@ namespace detail {
     };
     
     // try to assume (the last case) that vp is less in the direction (p1-p0) and both v0 and v1.
-    v0_v1_ts = start_velocity + end_velocity - delta_time * sign_p1_p0;
-    vm_p1_p0 = max_velocity * fabs(end_position - start_position);
+    v0_v1_ts_sa = start_velocity + end_velocity - 0.5 * delta_time * sign_p1_p0;
+    vm_p1_p0 = 0.5 * max_velocity * fabs(end_position - start_position);
     vsqr_avg = 0.5 * (start_velocity * start_velocity + end_velocity * end_velocity);
-    if(v0_v1_ts * v0_v1_ts > 4.0 * (vsqr_avg - vm_p1_p0)) {
+    descrim = v0_v1_ts_sa * v0_v1_ts_sa - 4.0 * (vsqr_avg - vm_p1_p0);
+    if(descrim > 0.0) {
       // then there is a real root to the quadratic equation.
-      double r1 = 0.5 * (v0_v1_ts + sqrt(v0_v1_ts * v0_v1_ts - 4.0 * (vsqr_avg - vm_p1_p0))) * sign_p1_p0;
-      if((r1 < start_velocity * sign_p1_p0) && (r1 < end_velocity * sign_p1_p0)) {
-        peak_velocity = sign_p1_p0 * r1;
+      peak_velocity = 0.5 * (v0_v1_ts_sa + sqrt(descrim));
+      if((peak_velocity * sign_p1_p0 <= start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 <= end_velocity * sign_p1_p0)) {
         return;
       };
-      r1 = 0.5 * (v0_v1_ts - sqrt(v0_v1_ts * v0_v1_ts - 4.0 * (vsqr_avg - vm_p1_p0))) * sign_p1_p0;
-      if((r1 < start_velocity * sign_p1_p0) && (r1 < end_velocity * sign_p1_p0)) {
-        peak_velocity = sign_p1_p0 * r1;
+      peak_velocity = 0.5 * (v0_v1_ts_sa - sqrt(descrim));
+      if((peak_velocity * sign_p1_p0 <= start_velocity * sign_p1_p0) && (peak_velocity * sign_p1_p0 <= end_velocity * sign_p1_p0)) {
         return;
       };
       // then, there was no suitable root in this region.
     };
+    
     // What the fuck!! This point should never be reached, unless the motion is completely impossible:
     peak_velocity = 0.0;
     return;
